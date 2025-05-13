199a46c561c9c7e870cc842191a7d7334058e4ac
mbernard <mbernard@stevens.edu>
2025-05-12 22:06:21 -0400

    Better seed

--- seed.js
+++ seed.js
@@ -6,8 +6,10 @@ const seedDatabase = async () => {
         const db = await dbConnection();
         await db.dropDatabase();
 
+        // Users
         const users = [
             {
+                _id: new ObjectId("60f5a3b8e1d3f2a5b8c9d001"),
                 firstName: "Colby",
                 lastName: "Foster",
                 userId: "foster",
@@ -16,90 +18,103 @@ const seedDatabase = async () => {
                 address: "1 Castle Point Terrace",
                 inHoboken: "yes",
                 state: "NJ",
-                signupDate: "05/06/2025",
+                signupDate: "05/06/2024",
                 lastLogin: "05/06/2025 08:24PM",
-                ratings: [{userId: "6820e128170e22aca1ca2c73", rating: 5}, {userId: "6820e128170e21aca1ca7c73", rating: 2}],
+                ratings: [
+                    { userId: "60f5a3b8e1d3f2a5b8c9d003", rating: 4 },
+                    { userId: "60f5a3b8e1d3f2a5b8c9d004", rating: 3 }
+                ],
                 ratingAverage: 3.5,
                 ratingCount: 2,
-                requests: [{endDate : "2025-05-14T13:57:35.284Z",
-                  extraComments : "I want this scooter",
-                  requestingUser : "ajfra",
-                  startDate: "2025-05-13T13:57:32.829Z",
-                  title: "Segway Ninebot ES4",
-                  vehicleId: "68221d78e3075c435680c814"
-                },
-                {endDate : "2025-05-14T13:57:35.284Z",
-                  extraComments : "I want this bike",
-                  requestingUser : "foster",
-                  startDate: "2025-05-13T13:57:32.829Z",
-                  title: "Mountain Bike Pro",
-                  vehicleId: "68221d78e3075c435680c815"
-                }
-              ],  
-                clients: ["ajfra"]
-              },
-              {
+                requests: [],
+                clients: []
+            },
+            {
+                _id: new ObjectId("60f5a3b8e1d3f2a5b8c9d002"),
                 firstName: "AJ",
                 lastName: "Francese",
                 userId: "ajfra",
                 password: "$2a$08$YewMkgeJKL9G9ytvJVfTbQOP",
                 email: "afrances@stevens.edu",
-                address: "2E Highview Avenue",
-                inHoboken: "no",
+                address: "493 Washington Street",
+                inHoboken: "yes",
                 state: "NJ",
-                signupDate: "05/06/2025",
+                signupDate: "05/06/2024",
                 lastLogin: "05/06/2025 08:24PM",
-                ratings: [{userId: "6820e128170e21aca1ca7c73", rating: 3}],
-                ratingAverage: 3,  
+                ratings: [
+                    { userId: "60f5a3b8e1d3f2a5b8c9d001", rating: 5 }
+                ],
+                ratingAverage: 5,
                 ratingCount: 1,
                 requests: [],
                 clients: []
-              },
-              {
-                firstName: "Micro",
-                lastName: "Mobile",
-                userId: "i_love_micromobile",
-                password: "$2a$08$ZfxNlhfKLM0H0zuwKWgUcRST",
-                email: "micromobile@gmail.com",
-                address: "800 Washington Street",
+            },
+            {
+                _id: new ObjectId("60f5a3b8e1d3f2a5b8c9d003"),
+                firstName: "Jack",
+                lastName: "Bulas",
+                userId: "DarklightNinja",
+                password: "$2a$08$YewMkgeJKL9G9ytvJVfTbQOP", 
+                email: "jbulas@stevens.edu",
+                address: "123 Jefferson Street",
                 inHoboken: "yes",
                 state: "NJ",
-                signupDate: "05/06/2025",
-                lastLogin: "05/06/2025 08:24PM",
+                signupDate: "01/10/2023",
+                lastLogin: "05/12/2025 09:00AM",
                 ratings: [],
-                ratingAverage: 0,  
+                ratingAverage: 0,
                 ratingCount: 0,
                 requests: [],
                 clients: []
-              }
+            },
+            {
+                _id: new ObjectId("60f5a3b8e1d3f2a5b8c9d004"),
+                firstName: "Harris",
+                lastName: "Hamid",
+                userId: "harrishamid",
+                password: "$2a$08$YewMkgeJKL9G9ytvJVfTbQOP", 
+                email: "hhamid@stevens.edu",
+                address: "456 Washington Street",
+                inHoboken: "yes",
+                state: "NJ",
+                signupDate: "05/10/2024",
+                lastLogin: "05/12/2025 09:05AM",
+                ratings: [
+                    { userId: "60f5a3b8e1d3f2a5b8c9d002", rating: 4 }
+                ],
+                ratingAverage: 4,
+                ratingCount: 1,
+                requests: [],
+                clients: []
+            }
         ];
-
         await db.collection('users').insertMany(users);
         console.log('Seeded users.');
 
+        // Posts
         const posts = [
             {
                 _id: new ObjectId("68221d78e3075c435680c814"),
                 postTitle: "Segway Ninebot ES4",
                 vehicleType: "Scooter",
                 vehicleTags: ["Electric", "Two Wheels"],
-                vehicleCondition: 4.5,
+                vehicleCondition: 3.8,
                 currentlyAvailable: true,
                 vehicleComments: [
-                  {
-                    _id: new ObjectId("68221d78e3075c435680c817"),
-                    Username: "foster",
-                    Name: "Colby Foster",
-                    commentDate: "12/12/2024",
-                    Body: "good scooter"
-                  },
-                  {
-                    _id: new ObjectId("68221d78e3075c435680c818"),
-                    Username: "ajfra",
-                    Name: "AJ Francese",
-                    commentDate: "3/5/2025",
-                    Body: "Great condition and easy to ride!"
-                  }
+                    {
+                        _id: new ObjectId("aaaaaaaaaaaaaaaaaaaaaaaa"),
+                        Username: "DarklightNinja",
+                        Name: "Jack Bulas",
+                        commentDate: "05/3/2025",
+                        Body: "Condition is lower than 3.8, but not terrible."
+                    },
+                    {
+                        _id: new ObjectId("bbbbbbbbbbbbbbbbbbbbbbbb"),
+                        Username: "harrishamid",
+                        Name: "Harris Hamid",
+                        commentDate: "05/9/2025",
+                        Body: "Does not work well on hills."
+                    }
                 ],
                 posterUsername: "foster",
                 posterName: "Colby Foster",
@@ -110,48 +125,48 @@ const seedDatabase = async () => {
                 location: "1 Castle Point Terrace",
                 image: "/public/uploads/seed1.png",
                 whenAvailable: [
-                  [], [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [1], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [], [], [1], [1], [1], [1], [],
-                  [], [], [], [], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [], [], [], [], [], [], [],
-                  [], [], [], [], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [1], [1], [1], [], [], [], [],
-                  [], [], [], [], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [1], [1], [1], [], [], [], [],
+                  [], [], [], [], [], [], [], [], [1], [1], [1], [1],
+                  [1], [1], [1], [1], [1], [1], [], [], [], [], [], [],
+
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [], [], [], [], [], [], [], [],
+
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [], [], [], [], [], [], [], [],
+
+                  [], [], [], [], [], [], [], [], [], [1], [1], [1], 
+                  [1], [], [], [], [], [], [], [], [], [], [], [],
+
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [], [], [], [], [], [], [], [],
+
+                  [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [1], [1], [1], [1], [1], [], [], [],
+                  
+                  [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [1], [1], [1], [1], [1], [], [], [],
                 ],
-                requests: [{endDate : "2025-05-14T13:57:35.284Z",
-                  extraComments : "I want this scooter",
-                  requestingUser : "ajfra",
-                  startDate: "2025-05-13T13:57:32.829Z",
-                  title: "Segway Ninebot ES4",
-                  vehicleId: "68221d78e3075c435680c814"
-                }],
+                requests: [],
                 taken: []
-              },
-              {
+            },
+            {
                 _id: new ObjectId("68221d78e3075c435680c815"),
                 postTitle: "Mountain Bike Pro",
                 vehicleType: "Bicycle",
-                vehicleTags: ["Off Road"],
-                vehicleCondition: 4.2,
+                vehicleTags: ["Off Road", "Two Wheels"],
+                vehicleCondition: 4.5,
                 currentlyAvailable: true,
                 vehicleComments: [
-                  {
-                    _id: new ObjectId("68221d78e3075c435680c816"),
-                    Username: "i_love_micromobile",
-                    Name: "Micro Mobile",
-                    commentDate: "5/5/2025",
-                    Body: "Smooth ride, great for trails"
-                  }
+                    {
+                        _id: new ObjectId("cccccccccccccccccccccccc"),
+                        Username: "foster",
+                        Name: "Colby Foster",
+                        commentDate: "05/5/2025",
+                        Body: "Great bike!"
+                    }
                 ],
-                posterUsername: "foster",
-                posterName: "Colby Foster",
+                posterUsername: "ajfra",
+                posterName: "AJ Francese",
                 maxRentalHours: 0,
                 maxRentalDays: 5,
                 hourlyCost: 0,
@@ -159,32 +174,128 @@ const seedDatabase = async () => {
                 location: "Hoboken Terminal",
                 image: "/public/uploads/seed2.jpg",
                 whenAvailable: [
-                  [], [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [1], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [], [], [1], [1], [1], [1], [],
-                  [], [], [], [], [], [], [], [], [], [],
-                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [], [], [], [], [], [], [],
-                  [], [], [], [], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [1], [1], [1], [], [], [], [],
-                  [], [], [], [], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1],
-                  [1], [1], [1], [1], [1], [1], [], [], [], [],
-                ],
-                requests: [{endDate : "2025-05-14T13:57:35.284Z",
-                  extraComments : "I want this bike",
-                  requestingUser : "foster",
-                  startDate: "2025-05-13T13:57:32.829Z",
-                  title: "Mountain Bike Pro",
-                  vehicleId: "68221d78e3075c435680c815"
-                }],
-                taken: []
-              }
-        ];
+                  [], [], [], [], [], [1], [1], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
 
+                  [], [], [], [], [], [1], [1], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [1], [1], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [1], [1], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [1], [1], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [], [], [], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [], [],
+                  
+                  [], [], [], [], [], [], [], [], [], [], [1], [1], 
+                  [1], [1], [1], [1], [1], [1], [1], [], [], [], [], [],
+                ],
+                requests: [],
+                taken: []
+            },
+            {
+                _id: new ObjectId("68221d78e3075c435680c817"),
+                postTitle: "Enuff Skully Skateboard",
+                vehicleType: "Skateboard",
+                vehicleTags: ["New"],
+                vehicleCondition: 4.8,
+                currentlyAvailable: true,
+                vehicleComments: [],
+                posterUsername: "harrishamid",
+                posterName: "Harris Hamid",
+                maxRentalHours: 6,
+                maxRentalDays: 2,
+                hourlyCost: 2,
+                dailyCost: 14,
+                location: "456 Washington Street",
+                image: "/public/uploads/seed3.jpg",
+                whenAvailable: [
+                  [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [1], [1], [1], [1], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [1], [1], [1], [1], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [1], [1], [1], [1], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [1], [],
+
+                  [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [], [], [], [1], [1], [1], [1], [1], [1], [],
+                  
+                  [], [], [], [], [], [], [], [], [], [], [], [],
+                  [], [], [1], [1], [1], [1], [1], [1], [1], [1], [1], [],
+                ],
+                requests: [],
+                taken: []
+            },
+            {
+                _id: new ObjectId("68221d78e3075c435680c816"),
+                postTitle: "Stauber Summit Snowboard",
+                vehicleType: "Other",
+                vehicleTags: ["Snow Gear"],
+                vehicleCondition: 3.2,
+                currentlyAvailable: true,
+                vehicleComments: [
+                    {
+                        _id: new ObjectId("dddddddddddddddddddddddd"),
+                        Username: "foster",
+                        Name: "Colby Foster",
+                        commentDate: "04/19/2025",
+                        Body: "How long is it?"
+                    },
+                    {
+                        _id: new ObjectId("eeeeeeeeeeeeeeeeeeeeeeee"),
+                        Username: "DarklightNinja",
+                        Name: "Jack Bulas",
+                        commentDate: "04/20/2025",
+                        Body: "The board is 128cm long."
+                    }
+                ],
+                posterUsername: "DarklightNinja",
+                posterName: "Jack Bulas",
+                maxRentalHours: 0,
+                maxRentalDays: 7,
+                hourlyCost: 0,
+                dailyCost: 18,
+                location: "123 Jefferson Street",
+                image: "/public/uploads/seed4.jpg",
+                whenAvailable: [
+                  [], [], [], [], [], [1], [1], [1], [1], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [], [],
+
+                  [], [], [], [], [], [1], [1], [1], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [], [],
+
+                  [], [], [], [], [], [1], [1], [1], [1], [1], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [], [],
+
+                  [], [], [], [], [], [1], [1], [], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [], [],
+
+                  [], [], [], [], [], [1], [1], [1], [], [], [], [],
+                  [], [], [], [], [], [], [1], [1], [1], [1], [], [],
+
+                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [1], [1], [1], [1], [1], [], [], [],
+                  
+                  [], [], [], [], [], [], [], [1], [1], [1], [1], [1], 
+                  [1], [1], [1], [1], [1], [1], [1], [], [], [], [], [],
+                ],
+                requests: [],
+                taken: []
+            },
+        ];
         await db.collection('posts').insertMany(posts);
         console.log('Seeded posts.');
 
@@ -195,5 +306,5 @@ const seedDatabase = async () => {
 };
 
 export {
-  seedDatabase
-}
+    seedDatabase
+};
